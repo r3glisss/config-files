30006f9fb2283b1a13074125c3933648e4fb4897
r3glisss <cyrbresch@protonmail.com>
2020-11-26 20:41:33 +0100

    Update configuration 8

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Scala dependencies
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
+
 sudo add-apt-repository universe
 
+# Update and upgrade
 sudo apt-get update
 sudo apt-get upgrade -y
 
@@ -54,6 +59,7 @@ sudo apt install -y \
 				python3-pip \
 				qemu \
 			 	sed \
+				scala \
 				texinfo \
 				tmux \
 				vbindiff \
